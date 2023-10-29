6b8b56423b5f24bad2b0c25f74322ebac74ea759
Pierre Qi <qiqinyuan@gmail.com>
2023-10-29 23:13:05 +0100

    Update code and man file

--- R/labbonus.R
+++ R/labbonus.R
@@ -17,27 +17,23 @@
 #' @param lambda Lambda
 #' @param qr use qr decomposition or not
 #' @return ridgereg object(list) contain all the calculated values regarding the linear regression
-#' @importFrom stats model.matrix pt
+#' @importFrom stats model.matrix pt sd
 #' @export 
-ridgereg <- function(formula, data, lambda, qr = FALSE) {
+ridgereg <- function(formula, data, lambda = 0, qr = FALSE) {
   
   # object to save all the meta data and calculated values
   obj <- list(formula = formula,
               data = data,
               lambda = lambda,
+              qr = qr,
               
-              data_name = substitute(data),
               coefficients = NULL,
-              residuals = NULL,
               fitted_values = NULL,
-              residual_var = NULL,
-              regressions_var_diag = NULL,
-              regressions_var = NULL,
-              t_values = NULL,
-              p_values = NULL,
               x_name = NULL,
               x_data = NULL,
-              df = NULL)
+              x_sd = NULL,
+              x_mean = NULL
+             )
   
   # set class name
   class(obj) <- "ridgereg"
@@ -45,6 +41,8 @@ ridgereg <- function(formula, data, lambda, qr = FALSE) {
   #----------------START OF INIT CODE----------------
   # get all the variables
   vars <- all.vars(formula)
+  
+  var_count <- length(vars)
   
   # get dependent variable name and data
   y_name <- vars[1]
@@ -55,20 +53,42 @@ ridgereg <- function(formula, data, lambda, qr = FALSE) {
   obj$x_name <- vars
   obj$x_data <- x_data
   
-  I = NULL
   
-  # normalize all covariates
+  # get row number of data
+  n <- nrow(x_data)
   
+  # normalize all co variates (X and Y)
+  x_data_norm <- scale(x_data[,-1])
+  x_data_norm <- cbind(x_data[,1],x_data_norm)
+  
+  x_sd <- apply(x_data[,-1], 2, sd)
+  obj$x_sd <- x_sd 
+  
+  x_mean <- colMeans(x_data[,-1])
+  obj$x_mean <- x_mean 
   
   # transpose of X_data and other calculation for later usage
-  x_data_t <- t(x_data)
-  xtx_inv <- solve(x_data_t %*% x_data)
+  x_data_norm_t <- t(x_data_norm)
+  xtx <- x_data_norm_t %*% x_data_norm
+  
+  # create a identity matrix
+  I <- diag(ncol(x_data))
   
   # calc beta_ridge
-  beta_ridge <- solve(xtx_inv + lambda %*% 1) %*% x_data_t %*% y_data
+  if(qr == FALSE){
+    beta_ridge <- (solve(xtx + lambda * I) %*% x_data_norm_t) %*% y_data 
+  }else{
+    QR <- qr(x_data)
+    Q <- qr.Q(QR)
+    R <- qr.R(QR)
+    beta_ridge <- (solve(t(R) %*% R  + lambda * I) %*% x_data_norm_t) %*% y_data
+  }
+  
+  obj$coefficients <- beta_ridge
   
   # calc fitted_values (y_hat)
-  fitted_values <- x_data %*% beta_ridge
+  fitted_values <- x_data_norm %*% beta_ridge
+  obj$fitted_values <- fitted_values
   
   #----------------END OF INIT CODE----------------
   obj
@@ -84,10 +104,12 @@ ridgereg <- function(formula, data, lambda, qr = FALSE) {
 print.ridgereg <- function(x,...) {
   obj <- x
   cat("Call:\n")
-  data_str <- paste("data = ",obj$data_name, ")", sep="")
-  formula_str <- paste("linreg(formula =",deparse(obj$formula))
+  data_str <- paste("data = ",obj$data_name, sep="")
+  formula_str <- paste("ridgereg(formula =",deparse(obj$formula))
+  qr_str <- paste("qr = ",obj$qr,sep="")
+  lambda_str <- paste("lambda = ",obj$lambda,")",sep="")
   
-  firstPart <- paste(formula_str,data_str, sep=", ")
+  firstPart <- paste(formula_str,data_str, qr_str, lambda_str, sep=", ")
   cat(firstPart)
   
   cat("\n\nCoefficients:\n")
@@ -97,103 +119,21 @@ print.ridgereg <- function(x,...) {
   cat(obj$coefficients)
 }
 
-#' get PNG file(used in plot())
-#' private function
-#' @param filename png file name used in plot
-get_png <- function(filename) {
-  grid::rasterGrob(readPNG(filename), interpolate = TRUE)
-}
-
-#' customize Liu theme (used in plot())
-#' private theme
-#'
-linkoping_theme <- theme(
-  panel.background = element_rect(fill = "white",
-                                  colour = "lightblue",
-                                  size = 0.5, linetype = "solid"),
-  panel.grid.major = element_line(size = 0.5, linetype = 'solid',
-                                  colour = "white"), 
-  panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                  colour = "white"),
-  plot.title = element_text(color="blue2", face="bold", size="14",hjust = 0.5)
-)
-
-#' plot 2 graphs with Liu Logo and customized liu theme.
-#'
-#' @param x ridgereg object
-#' @param ... graphical parameters to plot
-#' 
-#' @importFrom ggplot2 ggplot geom_path geom_point aes_string aes theme element_text element_rect element_line stat_summary labs theme_void annotation_custom
-#' @importFrom png readPNG
-#' @importFrom grid rasterGrob
-#' @importFrom gridExtra  grid.arrange
-#' 
-#' @export plot.linreg
-#' @export 
-#' 
-plot.ridgereg <- function(x, ...) {
-  obj <- x
-  
-  l <- get_png("LiU.png")
-  
-  # setup data frame for 1st graph
-  fitted_values <- as.data.frame(obj$fitted_values)
-  residuals <- as.data.frame(obj$residuals)
-  df_g1 <- data.frame(fitted_values,residuals)
-  
-  # draw 1st graph
-  p1 <- ggplot2::ggplot(data = df_g1,mapping = aes_string(x = names(df_g1)[1], y = names(df_g1)[2])) +
-    geom_point(shape = 21) +
-    stat_summary(fun="median",geom="line",linetype = "dashed",colour="red") +
-    linkoping_theme +
-    labs(
-      title = "Residuals vs Fitted",
-      x = "Fitted values \n lm(Petal.Length ~ Species)",
-      y = "Residuals"
-    )
-  
-  # calc the mean of residuals-> miu and set up data frame for 2nd graph
-  miu <- mean(residuals[[names(residuals)[1]]])
-  sigma <- sqrt(sum((residuals - miu) ^ 2) / nrow(residuals))
-  
-  residuals_new <- abs((residuals - miu) / sigma)
-  df_g2 <- data.frame(fitted_values,residuals_new)
-  
-  # draw 2nd graph
-  p2 <- ggplot2::ggplot(data = df_g2,mapping = aes_string(x = names(df_g2)[1], y = names(df_g2)[2])) +
-    geom_point(shape = 21) +
-    stat_summary(fun="median",geom="line",linetype = "dashed",colour="red") +
-    linkoping_theme + 
-    labs(
-      title = "Scale Location",
-      x = "Fitted values \n lm(Petal.Length ~ Species)",
-      y = expression(paste(sqrt(abs("Standard Residuals"))))
-    )
-  
-  # draw 3rd graph (LOGO)
-  p3 <- ggplot(mapping = aes(x = 0:1, y = 0.1)) +
-    theme_void() +
-    annotation_custom(l, xmin = .8, xmax = 1)
-  grid.arrange(p1, p2, p3,heights = c(.46,.46,.08))
-}
-
-#' get fitted value
+#' predict  value
 #'
 #' @param obj ridgereg object
-#' @export pred
+#' @param x value X
+#' @return predicted value
+#' @export predict
 #' @export
-pred <- function(obj){
-  return(obj$fitted_values)
-}
-
-#' get residuals
-#'
-#' @param obj ridgereg object
-#' @export resid
-#' @export
-
-resid <- function(obj){
-  obj$residuals
+#' 
+predict <- function(obj,x){
+  if (is.null(x)){
+    stop("input is null")
+  }
+  x_norm <- scale(x)
+  prediction <- as.matrix(cbind(1,x_norm)) * obj$coefficients
+  return(prediction)
 }
 
 #' get coefficients values
@@ -204,57 +144,4 @@ resid <- function(obj){
 
 coef <- function(obj){
   obj$coefficients
-}
-
-
-#' print out summary which is simiar as lm()
-#'
-#' @param object ridgereg object
-#' @param ... other objects
-#' @importFrom methods show
-#' @export summary.ridgereg
-#' @export 
-summary.ridgereg <- function(object, ...){
-  obj <- object
-  cat("Call:\n")
-  data_str <- paste("data = ",obj$data_name, ")", sep="")
-  formula_str <- paste("linreg(formula =",deparse(obj$formula))
-  firstPart <- paste(formula_str,data_str, sep=", ")
-  cat(firstPart)
-  cat("\n\n")
-  
-  cat("Coefficients:\n")
-  star <- lapply(obj$p_values[1],check_p_value)
-  secondPart <- data.frame(obj$coefficients,
-                           sqrt(obj$regressions_var_diag),
-                           obj$t_values,
-                           formatC(obj$p_values, format = "e", digits = 2),
-                           star
-  )
-  
-  names(secondPart) <- c("Estimate","Std. Error","t value","Pr(>|t|)"," ")
-  show(secondPart)
-  
-  cat("\n\n")
-  # Residual standard error
-  residual_standard_error_str <- paste("Residual standard error: ",sqrt(obj$residual_var)," on ",obj$df , " degrees of freedom",sep="")
-  cat(residual_standard_error_str)
-}
-
-#' return confidence indicator[. * ** ***] according to the value X
-#'
-#' @param x numeric
-#' 
-check_p_value <- function(x){
-  if (x > 0.1){
-    return("")
-  }else if (x > 0.05){
-    return(".")
-  }else if (x > 0.01){
-    return("*")
-  }else if (x > 0.001){
-    return("**")
-  }else{
-    return("***")
-  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 732A94 Advanced Programming in R Lab Bonus
+
+[![R-CMD-check](https://github.com/qqyfly/732A94_Advanced_Programming_in_R_Lab_Bonus/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/qqyfly/732A94_Advanced_Programming_in_R_Lab_Bonus/actions/workflows/R-CMD-check.yaml)
