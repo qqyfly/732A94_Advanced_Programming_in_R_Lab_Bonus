df9232db1eaa5a97ba07478399555924862043fc
Pierre Qi <qiqinyuan@gmail.com>
2023-10-29 13:33:43 +0100

    add package document

---
+++ R/labbouns-package.R
@@ -0,0 +1,11 @@
+#' Lab Bonus of Advanced Programming in R
+#'
+#' This package include following S3 class\cr
+#' \cr 
+#' 1. ridgereg - a class provide linear regression  that is similar as lm()\cr 
+#' \cr
+
+#' @name labbonus-package
+#' @aliases labbonus
+#' @docType package
+NULL
